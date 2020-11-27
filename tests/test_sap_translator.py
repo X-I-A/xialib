eb8a4bbecab24e6a29dcddf8a421fceee8009a02
Soral <cx.soral@gmail.com>
2020-11-27 23:14:53 +0100

    ddic header translator

--- tests/test_sap_translator.py
+++ tests/test_sap_translator.py
@@ -7,8 +7,12 @@ slt_init_load = [{'_RECNO': 1000, 'MANDT': '100', 'BUKRS': '1001'}]
 slt_transfer = [{'_RECNO': 1000, 'MANDT': '100', 'BUKRS': '1001', 'IUUT_OPERAT_FLAG': 'U'}]
 slt_init_header = {'age': '2', 'data_spec': 'slt', 'data': slt_init_load}
 slt_transfer_header = {'age': '2', 'data_spec': 'slt', 'data': slt_transfer}
-ddic_data = [{'DUMMY': '123'}]
 ddic_header = {'age': '1', 'data_spec': 'ddic', 'data': slt_init_load}
+ddic_data = [{'FIELDNAME': 'F1', 'INTTYPE': 'C', 'LENG': 4, 'FIELDTEXT': 'Field 1'},
+             {'FIELDNAME': 'F2', 'INTTYPE': 'N', 'LENG': 4, 'FIELDTEXT': 'Field 2'},
+             {'FIELDNAME': 'F3', 'INTTYPE': 'P', 'LENG': 15, 'DECIMALS': 4, 'FIELDTEXT': 'Field 3'},
+             {'FIELDNAME': 'F4', 'INTTYPE': 'D', 'FIELDTEXT': 'Field 4'},
+             {'FIELDNAME': 'F5', 'INTTYPE': 'T', 'FIELDTEXT': 'Field 5'}, ]
 
 @pytest.fixture()
 def translator():
@@ -31,7 +35,10 @@ def test_transfer_header(translator):
         assert '_OP' in result_line
 
 def test_ddic_header(translator):
-    translator.compile(ddic_header, ddic_header['data'])
-    for line in ddic_header['data']:
-        result_line = translator.get_translated_line(line)
-        assert result_line == line
+    translator.compile(ddic_header, ddic_data)
+    ddic_translated = [translator.get_translated_line(line) for line in ddic_data]
+    assert ddic_translated[0].get('type_chain') == ['char', 'c_4']
+    assert ddic_translated[1].get('type_chain') == ['char', 'c_4', 'n_4']
+    assert ddic_translated[2].get('type_chain') == ['char', 'c_16', 'd_15_4']
+    assert ddic_translated[3].get('type_chain') == ['char', 'c_8', 'date']
+    assert ddic_translated[4].get('type_chain') == ['char', 'c_6', 'time']
--- xialib/translators/sap_translator.py
+++ xialib/translators/sap_translator.py
@@ -6,12 +6,51 @@ class SapTranslator(Translator):
     """
     spec_list = ['slt', 'ddic']
 
+    ddic_dict = {
+        'C': ['c_@leng@', None, None],
+        'N': ['n_@leng@', None, None],
+        'D': ['date', 'YYYYMMDD', None],
+        'T': ['time', 'HHMMSS', None],
+        'X': ['blob', None, 'b16'],
+        'I': ['i_@leng@', None, None],
+        'b': ['i_@leng@', None, None],
+        's': ['i_@leng@', None, None],
+        'P': ['d_@leng@_@decimals@', None, None],
+        'F': ['float', None, None],
+        'g': ['char', None, None],
+        'y': ['blob', None, None],
+        'u': ['json', None, None],
+        'v': ['json', None, None],
+        'h': ['json', None, None],
+        'V': ['char', None, None],
+        'r': ['json', None, None],
+        'l': ['json', None, None],
+        'a': ['float', None, None],
+        'e': ['double', None, None],
+        'j': ['c_1', None, None],
+        'k': ['c_1', None, None],
+        'z': ['json', None, None],
+        '8': ['i_8', None, None],
+    }
+
     def __init__(self):
         super().__init__()
         self.line_oper = dict()
 
     def _get_ddic_line(self, line: dict, **kwargs):
-        return line
+        new_line = {'_' + key: value for key, value in line.items()}
+        new_line['field_name'] = new_line['_FIELDNAME']
+        new_line['key_flag'] = new_line.get('_KEYFLAG', '')
+        new_line['description'] = new_line.get('_FIELDTEXT', '')
+        ddic_parse = self.ddic_dict.get(new_line['_INTTYPE']).copy()
+        if '@leng@' in ddic_parse[0]:
+            ddic_parse[0] = ddic_parse[0].replace('@leng@', str(new_line['_LENG']))
+        if '@decimals@' in ddic_parse[0]:
+            ddic_parse[0] = ddic_parse[0].replace('@decimals@', str(new_line['_DECIMALS']))
+        new_line['type_chain'] = self.get_type_chain(ddic_parse[0], ddic_parse[1])
+        new_line['format'] = ddic_parse[1]
+        new_line['encode'] = ddic_parse[2]
+        return new_line
 
     def _get_slt_line(self, line: dict, **kwargs):
         line['_AGE'] = int(kwargs['age'])
